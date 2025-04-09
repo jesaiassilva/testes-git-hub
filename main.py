a867fa5e101b3b46c085bd1c6c278f73429215ec
Jesaias Carvalho Pereira Silva <jesaiassilva@isi-er.com.br>
2025-04-09 13:48:44 -0300

    fist commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils.calculos import fatorial
+
+a = int(input("Qal valor calcular o fatorial?"))
+
+print(f"O fatoprial de {a} é {fatorial(a)}")
---
+++ utils/calculos.py
@@ -0,0 +1,6 @@
+def fatorial(a):
+    f = 1
+    for i in range(a):
+        f *= i+1
+    return f
+
